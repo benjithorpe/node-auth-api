dafca1a64595ccc18f73d027e6b3e8081a8b04dd
benjithorpe <benthorpe.dev@gmail.com>
2022-09-04 12:36:02 +0000

    add dummy posts in the post routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 require('dotenv/config');
 
 const authRoute = require('./routes/auth.js');
+const postsRoute = require('./routes/posts.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -16,6 +17,7 @@ app.use(express.json());
 
 // Middlewares
 app.use('/api/user', authRoute);
+app.use('/api/posts', postsRoute);
 
 app.get('/', (req, res) => res.send("check '/api/user' for the user API"));
 
